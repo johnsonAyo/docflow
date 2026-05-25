c779e0f990a8a7edd99f9ecc51a164324f29864f
Jay <Johnsonafuye@gmail.com>
2026-05-25 06:57:27 +0100

    refactor: remove unused onRunWorkflow prop from WorkflowOverview component

--- apps/web/src/components/WorkspaceComponents/WorkflowOverview/index.tsx
+++ apps/web/src/components/WorkspaceComponents/WorkflowOverview/index.tsx
@@ -16,7 +16,6 @@ export function WorkflowOverview({
   onCreateWorkflow,
   onDeleteWorkflow,
   onOpenWorkflow,
-  onRunWorkflow,
   savedWorkflows,
 }: WorkflowOverviewProps) {
   const workflowRows = savedWorkflows.map((workflow) => ({
